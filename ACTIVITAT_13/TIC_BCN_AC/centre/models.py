bff99f296f2ff83efccff915bff2868e7a2ae057
Achraf Chakir <2023_achraf.chakir@iticbcn.cat>
2025-02-03 18:13:46 +0100

    proves

--- ACTIVITAT_13/TIC_BCN_AC/centre/models.py
+++ ACTIVITAT_13/TIC_BCN_AC/centre/models.py
@@ -5,6 +5,16 @@ class Alumno(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     edad = models.IntegerField()
+    modulos = models.TextField()  
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    cursos = models.TextField()  
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
